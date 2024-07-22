1ebe58d083e8ae22bddc89e10598136cce50c106
chosybaby <157858612+chosybaby@users.noreply.github.com>
2024-07-22 02:46:53 +0200

    Update usermods_list.cpp

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -204,6 +204,11 @@
 #ifdef USERMOD_STAIRCASE_WIPE
 #include "../usermods/stairway_wipe_basic/stairway-wipe-usermod-v2.h"
 #endif
+
+#ifdef USERMOD_WIPE_IN
+#include "../usermods/WIPE_IN/usermod_wipe_in.h"
+#endif
+
 void registerUsermods()
 {
 /*
@@ -386,5 +391,9 @@ void registerUsermods()
 
   #ifdef USERMOD_STAIRCASE_WIPE
   usermods.add(new StairwayWipeUsermod());
+  #endif
+
+  #ifdef USERMOD_WIPE_IN
+  usermods.add(new WipeInUsermod());
   #endif
 }
